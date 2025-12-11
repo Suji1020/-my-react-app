2d2a67b5ae7e3d83cc7f03820216ed4bd8ac6a95
Sujitha <sujithathirumuru@gmail.com>
2025-12-11 18:43:10 +0530

    Operator

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar"
+// import Home from "./components/Home";
+// import About from "./components/About";
 import Home from "./components/Home";
 import About from "./components/About";
 import Contact from "./components/Contact";

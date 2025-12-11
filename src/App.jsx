fc1a3a5a2cce9f4d0219834bfdd1d67ee07e4bce
Sujitha <sujithathirumuru@gmail.com>
2025-12-11 18:37:51 +0530

    Formjsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,19 @@
 import React from "react";
-<<<<<<< HEAD
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar"
 import Home from "./components/Home";
 import About from "./components/About";
 import Contact from "./components/Contact";
-=======
 import { useState } from "react";
 import Useeffect from "./components/Useeffect";
 import Ternaryoperator from "./components/Ternaryoperator";
 import Firstcomponent from "./components/Firstcomponent";
 import FormEvents from "./components/FormEvents";
->>>>>>> 82cb665 (FormEvents)
+
 
 const App = () => {
   return (
     <div>
-<<<<<<< HEAD
      <Navbar/>
        
       <Routes>
@@ -24,16 +21,11 @@ const App = () => {
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-=======
       <FormEvents/>
 
->>>>>>> 82cb665 (FormEvents)
+
     </div>
   );
 }
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 82cb665 (FormEvents)
 export default App;
